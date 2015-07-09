fa3ca725dd280dfda450ab8d340645561bf274a2
RuanCong <1308811723@qq.com>
2015-07-09 14:13:34 +0800

    增加了TestValidation

--- spring/src/com/test/TestValidation.java
+++ spring/src/com/test/TestValidation.java
@@ -1,13 +1,22 @@
 package com.test;
 
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.DataBinder;
+
 import com.beans.Person;
 import com.validations.PersonValidation;
 
 public class TestValidation {
 	public static void main(String[] args) {
 		Person p = new Person();
+		p.setAge(-2);
 		PersonValidation pv = new PersonValidation();
 
-
+		DataBinder binder = new DataBinder(p);
+		binder.setValidator(pv);
+		
+		binder.validate();
+		BindingResult bResult = binder.getBindingResult();
+		System.out.println(bResult.getFieldError().getCode());
 	}
 }
